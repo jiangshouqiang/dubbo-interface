9c22cc8d69c6b4979427f382384d89927696e60f
jiangshouqiang <284923424@qq.com>
2016-12-13 09:40:56 +0800

    Initial commit

---
+++ src/main/java/com/gr/jiang/mod/hello/UserService.java
@@ -0,0 +1,12 @@
+package com.gr.jiang.mod.hello;
+
+import java.util.List;
+
+/**
+ * Created by jiang on 2016/12/12.
+ */
+public interface UserService {
+    String sayHello(String name);
+
+    public List getUsers();
+}
